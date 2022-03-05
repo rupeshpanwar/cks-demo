a1abd3a58adde0d432361d2121c29c82e39e5462
Jon Doe <jondoe@doe.com>
2022-03-05 18:42:00 +0800

    Networl policy default deny n allow

--- git-commit.sh
+++ git-commit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 git add .
-git commit -m 'Container under d hood'
+git commit -m 'Networl policy default deny n allow'
 git push origin main
+
 
 
  #git push https://<personal-token>@github.com/rupeshpanwar/cks-demo.git
